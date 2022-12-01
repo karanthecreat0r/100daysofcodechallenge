6a4837f710e57e3b1f4186c6e927c66deb6f7a26
karansparrow <karansparrow1226@gmail.com>
2022-12-01 14:58:21 +0530

    100daycode

---
+++ Day-33 React State/sample.js
@@ -0,0 +1,63 @@
+// import React from "react";
+
+// function App() {
+// return <p> Hello</p>;
+// }
+
+// export default App;
+
+// --------------------------------------------
+
+
+
+// import React from "react";
+
+// var strike = false;
+
+
+// function App() {
+// return <p> style = {{textDecoration: "line-through"}} Hello</p>;
+// }
+
+// export default App;
+
+// --------------------------------------------
+
+
+// import React from "react";
+
+// var strike = false;
+
+// const strikeThrough = {textDecoration: "line-through"}
+
+// function App() {
+// return <p> style = {strike? strikeThrough:null} Hello</p>;
+// }
+
+// export default App;
+
+// --------------------------------------------
+
+
+
+// import React from "react";
+
+// var strike = false;
+
+// const strikeThrough = {textDecoration: "line-through"}
+
+// function App() {
+// return <p> style = {strike && strikeThrough} Hello</p>;
+// }
+
+// export default App;
+
+// --------------------------------------------
+
+// import React from "react";
+// import ReactDom from "react-dom";
+// import App from "./App";
+
+// ReactDOM.render(<App />. document.getElementById("root"));
+
+// document.getElementById("root").style.textDecoration = "line-through";
